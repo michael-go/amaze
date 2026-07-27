e50e8c9309c39cccedb3d40a169bf910507a8fda
Michael Gokhman <michael.gokhman@yahoo.com>
2026-07-27 12:48:01 +0300

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./components/App.jsx";
 import { I18nProvider } from "./lib/i18n.jsx";
 import "./index.css";
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <I18nProvider>
       <App />
+      <Analytics />
     </I18nProvider>
   </StrictMode>,
 );
